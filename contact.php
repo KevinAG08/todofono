74ebccfd5b3f666364a695ffa0e84366620783d5
KevinAG08 <75378802@continental.edu.pe>
2021-09-24 01:00:10 -0500

    up

---
+++ contact.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="zxx" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hmart - Contact page</title>
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Hmart-Smart Product eCommerce html Template">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- CSS
+    ============================================ -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/font.awesome.css" />
+    <link rel="stylesheet" href="assets/css/pe-icon-7-stroke.css" />
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/venobox.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Minify Version -->
+    <!-- <link rel="stylesheet" href="assets/css/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css"> -->
+</head>
+
+<body>
+    <div class="main-wrapper">
+        
+        <?php require ('header.php'); ?>
+
+                <!-- OffCanvas Menu End -->
+                <div class="offcanvas-social mt-auto">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-google"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-youtube"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- OffCanvas Menu End -->
+        <!-- breadcrumb-area start -->
+        <div class="breadcrumb-area">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-12 text-center">
+                        <h2 class="breadcrumb-title">Contact Us</h2>
+                        <!-- breadcrumb-list start -->
+                        <ul class="breadcrumb-list">
+                            <li class="breadcrumb-item"><a href="V_index.php">Home</a></li>
+                            <li class="breadcrumb-item active">Contact</li>
+                        </ul>
+                        <!-- breadcrumb-list end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- breadcrumb-area end -->
+        <!-- Contact Area Start -->
+        <div class="contact-area">
+            <div class="container">
+                <div class="contact-wrapper">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="contact-form">
+                                <div class="contact-title mb-30">
+                                    <h2 class="title">Send A Quest</h2>
+                                </div>
+                                <form class="contact-form-style" id="contact-form" action="https://whizthemes.com/nazmul/php/mail.php" method="post">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <input name="name" placeholder="Name*" type="text" />
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <input name="email" placeholder="Email*" type="email" />
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <input name="subject" placeholder="Subject*" type="text" />
+                                        </div>
+                                        <div class="col-lg-12 text-center">
+                                            <textarea name="message" placeholder="Your Message*"></textarea>
+                                            <button class="btn btn-primary" type="submit">Send Message</button>
+                                        </div>
+                                    </div>
+                                </form>
+                                <p class="form-messege"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="contact-info">
+                                <div class="single-contact">
+                                    <div class="icon-box">
+                                        <img src="assets/images/icons/contact-1.png" alt="">
+                                    </div>
+                                    <div class="info-box">
+                                        <h5 class="title">Address</h5>
+                                        <p>Your address goes here. <br>
+                                        123 Your Location</p>
+                                    </div>
+                                </div>
+                                <div class="single-contact">
+                                    <div class="icon-box">
+                                        <img src="assets/images/icons/contact-2.png" alt="">
+                                    </div>
+                                    <div class="info-box">
+                                        <h5 class="title">Phone No</h5>
+                                        <p><a href="tel:0123456789">+012 345 67 89</a></p>
+                                        <p><a href="tel:0123456789">+012 345 67 89</a></p>
+                                    </div>
+                                </div>
+                                <div class="single-contact m-0">
+                                    <div class="icon-box">
+                                        <img src="assets/images/icons/contact-3.png" alt="">
+                                    </div>
+                                    <div class="info-box">
+                                        <h5 class="title">Email/Web</h5>
+                                        <p><a href="mailto:demo@example.com">demo@example.com</a></p>
+                                        <p><a href="https://www.example.com">www.example.com</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact Area End -->
+        <!-- map Area Start -->
+        <div class="contact-map">
+            <div id="mapid">
+                <div class="mapouter">
+                    <div class="gmap_canvas">
+                        <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=121%20King%20St%2C%20Melbourne%20VIC%203000%2C%20Australia&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+                        <a href="https://sites.google.com/view/maps-api-v2/mapv2"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- map Area End -->
+        <!-- Footer Area Start -->
+        <div class="footer-area">
+            <div class="footer-container">
+                <div class="footer-top">
+                    <div class="container">
+                        <div class="row">
+                            <!-- Start single blog -->
+                            <div class="col-md-6 col-lg-3 mb-md-30px mb-lm-30px">
+                                <div class="single-wedge">
+                                    <div class="footer-logo">
+                                        <a href="V_index.php"><img src="assets/images/logo/footer-logo.png" alt=""></a>
+                                    </div>
+                                    <p class="about-text">Lorem ipsum dolor sit amet consl adipisi elit, sed do eiusmod templ incididunt ut labore
+                                    </p>
+                                    <ul class="link-follow">
+                                        <li>
+                                            <a class="m-0" title="Twitter" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-facebook"
+                                                aria-hidden="true"></i></a>
+                                        </li>
+                                        <li>
+                                            <a title="Tumblr" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-tumblr" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a title="Facebook" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a title="Instagram" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- End single blog -->
+                            <!-- Start single blog -->
+                            <div class="col-md-6 col-lg-3 col-sm-6 mb-lm-30px pl-lg-60px">
+                                <div class="single-wedge">
+                                    <h4 class="footer-herading">Services</h4>
+                                    <div class="footer-links">
+                                        <div class="footer-row">
+                                            <ul class="align-items-center">
+                                                <li class="li"><a class="single-link" href="my-account.html">My Account</a></li>
+                                                <li class="li"><a class="single-link" href="contact.html">Contact</a></li>
+                                                <li class="li"><a class="single-link" href="cart.html">Shopping cart</a></li>
+                                                <li class="li"><a class="single-link" href="shop-left-sidebar.html">Shop</a></li>
+                                                <li class="li"><a class="single-link" href="login.html">Services Login</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End single blog -->
+                            <!-- Start single blog -->
+                            <div class="col-md-6 col-lg-3 col-sm-6 mb-lm-30px pl-lg-40px">
+                                <div class="single-wedge">
+                                    <h4 class="footer-herading">My Account</h4>
+                                    <div class="footer-links">
+                                        <div class="footer-row">
+                                            <ul class="align-items-center">
+                                                <li class="li"><a class="single-link" href="my-account.html">My Account</a></li>
+                                                <li class="li"><a class="single-link" href="contact.html">Contact</a></li>
+                                                <li class="li"><a class="single-link" href="cart.html">Shopping cart</a></li>
+                                                <li class="li"><a class="single-link" href="shop-left-sidebar.html">Shop</a></li>
+                                                <li class="li"><a class="single-link" href="login.html">Services Login</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End single blog -->
+                            <!-- Start single blog -->
+                            <div class="col-md-6 col-lg-3 col-sm-12">
+                                <div class="single-wedge">
+                                    <h4 class="footer-herading">Contact Info</h4>
+                                    <div class="footer-links">
+                                        <!-- News letter area -->
+                                        <p class="address">Address: Your Address Goes Here.</p>
+                                        <p class="phone">Phone/Fax:<a href="tel:0123456789"> 0123456789</a></p>
+                                        <p class="mail">Email:<a href="mailto:demo@example.com"> demo@example.com</a></p>
+                                        <p class="mail"><a href="https://demo@example.com"> demo@example.com</a></p>
+                                        <!-- News letter area  End -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End single blog -->
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                    <div class="container">
+                        <div class="line-shape-top line-height-1">
+                            <div class="row flex-md-row-reverse align-items-center">
+                                <div class="col-md-6 text-center text-md-end">
+                                    <div class="payment-mth"><a href="#"><img class="img img-fluid" src="assets/images/icons/payment.png" alt="payment-image"></a></div>
+                                </div>
+                                <div class="col-md-6 text-center text-md-start">
+                                    <p class="copy-text"> © 2021 <strong>Hmart</strong> Made With <i class="fa fa-heart"
+                                        aria-hidden="true"></i> By <a class="company-name" href="https://themeforest.net/user/codecarnival/portfolio">
+                                            <strong> Codecarnival </strong></a>.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer Area End -->
+    </div>
+    <!-- Global Vendor, plugins JS -->
+    <!-- JS Files
+    ============================================ -->
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/scrollUp.js"></script>
+    <script src="assets/js/plugins/venobox.min.js"></script>
+    <script src="assets/js/plugins/jquery-ui.min.js"></script>
+    <script src="assets/js/plugins/mailchimp-ajax.js"></script>
+
+    <!-- Minify Version -->
+    <!-- <script src="assets/js/vendor.min.js"></script>
+    <script src="assets/js/plugins.min.js"></script>
+    <script src="assets/js/main.min.js"></script> -->
+
+    <!--Main JS (Common Activation Codes)-->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -31,7 +31,7 @@
                     <div class="row justify-content-between align-items-center">
                         <div class="col-lg-3 col">
                             <div class="header-logo">
-                                <a href="index.php"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
+                                <a href="V_index.php"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
                             </div>
                         </div>
                         <div class="col-lg-6 d-none d-lg-block">
@@ -69,7 +69,7 @@
                     <div class="row justify-content-between align-items-center">
                         <div class="col-lg-3 col">
                             <div class="header-logo">
-                                <a href="index.php"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
+                                <a href="V_index.php"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
                             </div>
                         </div>
                         <div class="col-lg-6 d-none d-lg-block">
@@ -107,7 +107,7 @@
                     <div class="header-nav">
                         <div class="main-menu position-relative">
                             <ul>
-                                <li><a href="about.php">Acerca de nosotros</a></li>
+                                <li><a href="about.php">Sobre Nosotros</a></li>
                                 
                                 <li><a href="shop.php">Tienda</a></li>
                                 
@@ -135,9 +135,6 @@
         <div class="offcanvas-overlay"></div>
         <!-- offcanvas overlay end -->
         <!-- OffCanvas Wishlist Start -->
-
-
-
 
         <div id="offcanvas-wishlist" class="offcanvas offcanvas-wishlist">
             <div class="inner">
@@ -240,106 +237,10 @@
             <div class="inner customScroll">
                 <div class="offcanvas-menu mb-4">
                     <ul>
-                        <li><a href="#"><span class="menu-text">Home</span></a>
-                            <ul class="sub-menu">
-                                <li><a href="index.html"><span class="menu-text">Home 1</span></a></li>
-                                <li><a href="index-2.html"><span class="menu-text">Home 2</span></a></li>
-                            </ul>
-                        </li>
-                        <li><a href="about.html">About</a></li>
-                        <li>
-                            <a href="#"><span class="menu-text">Pages</span></a>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#"><span class="menu-text">Inner Pages</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="404.html">404 Page</a></li>
-                                        <li><a href="order-tracking.html">Order Tracking</a></li>
-                                        <li><a href="faq.html">Faq Page</a></li>
-                                        <li><a href="coming-soon.html">Coming Soon Page</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="menu-text"> Other Shop Pages</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="cart.html">Cart Page</a></li>
-                                        <li><a href="checkout.html">Checkout Page</a></li>
-                                        <li><a href="compare.html">Compare Page</a></li>
-                                        <li><a href="wishlist.html">Wishlist Page</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="menu-text">Related Shop Page</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="my-account.html">Account Page</a></li>
-                                        <li><a href="login.html">Login & Register Page</a></li>
-                                        <li><a href="empty-cart.html">Empty Cart Page</a></li>
-                                        <li><a href="thank-you-page.html">Thank You Page</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><span class="menu-text">Shop</span></a>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#"><span class="menu-text">Shop Page</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="shop-3-column.html">Shop 3 Column</a></li>
-                                        <li><a href="shop-4-column.html">Shop 4 Column</a></li>
-                                        <li><a href="shop-left-sidebar.html">Shop Left Sidebar</a></li>
-                                        <li><a href="shop-right-sidebar.html">Shop Right Sidebar</a></li>
-                                        <li><a href="shop-list-left-sidebar.html">Shop List Left Sidebar</a>
-                                        </li>
-                                        <li><a href="shop-list-right-sidebar.html">Shop List Right Sidebar</a>
-                                        </li>
-                                        <li><a href="cart.html">Cart Page</a></li>
-                                        <li><a href="checkout.html">Checkout Page</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="menu-text">product Details Page</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="single-product.html">Product Single</a></li>
-                                        <li><a href="single-product-variable.html">Product Variable</a></li>
-                                        <li><a href="single-product-affiliate.html">Product Affiliate</a></li>
-                                        <li><a href="single-product-group.html">Product Group</a></li>
-                                        <li><a href="single-product-tabstyle-2.html">Product Tab 2</a></li>
-                                        <li><a href="single-product-tabstyle-3.html">Product Tab 3</a></li>
-                                        <li><a href="single-product-slider.html">Product Slider</a></li>
-                                        <li><a href="single-product-gallery-left.html">Product Gallery Left</a>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="menu-text">Single Product Page</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="single-product-gallery-right.html">Product Gallery
-                                                Right</a> </li>
-                                        <li><a href="single-product-sticky-left.html">Product Sticky Left</a>
-                                        </li>
-                                        <li><a href="single-product-sticky-right.html">Product Sticky Right</a>
-                                        </li>
-                                        <li><a href="compare.html">Compare Page</a></li>
-                                        <li><a href="wishlist.html">Wishlist Page</a></li>
-                                        <li><a href="my-account.html">Account Page</a></li>
-                                        <li><a href="login.html">Login & Register Page</a></li>
-                                        <li><a href="empty-cart.html">Empty Cart Page</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><span class="menu-text">Blog</span></a>
-                            <ul class="sub-menu">
-                                <li><a href="blog-grid.html">Blog Grid Page</a></li>
-                                <li><a href="blog-grid-left-sidebar.html">Grid Left Sidebar</a></li>
-                                <li><a href="blog-grid-right-sidebar.html">Grid Right Sidebar</a></li>
-                                <li><a href="blog-list.html">Blog List Page</a></li>
-                                <li><a href="blog-list-left-sidebar.html">List Left Sidebar</a></li>
-                                <li><a href="blog-list-right-sidebar.html">List Right Sidebar</a></li>
-                                <li><a href="blog-single.html">Blog Single Page</a></li>
-                                <li><a href="blog-single-left-sidebar.html">Single Left Sidebar</a></li>
-                                <li><a href="blog-single-right-sidebar.html">Single Right Sidbar</a>
-                            </ul>
-                        </li>
+                        <li><a href="about.php">Sobre Nosotros</a></li>
+                        
+                        <li><a href="shop.php">Tienda</a></li>
+
                         <li><a href="contact.html">Contact Us</a></li>
                     </ul>
                 </div>
